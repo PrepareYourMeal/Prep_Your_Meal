76eaf71c0c41b7a5da5c15e03de3c677061e9d12
Tanvi-412 <49460152+Tanvi-412@users.noreply.github.com>
2019-10-21 23:53:04 -0400

    google and facebook login

---
+++ client/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	},
+
+	'googleAuth' : {
+		'clientID': '328199517198-o1i105t2557febc0ccl2fgnjgfrula8j.apps.googleusercontent.com',
+		'clientSecret': 'DlHZYFXWAVKHvPLw8EjvTWS7',
+		'callbackURL': 'http://ocalhost:8080/auth/google/callback'
+	}
+}
